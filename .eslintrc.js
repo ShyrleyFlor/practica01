81b2aac3add0940b4f7302507e7cb649d1ca45f3
ShyrleyFlor <shyrbe.20@gmail.com>
2024-09-01 11:10:40 -0400

    CONFIGURACIONES

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native', // Reglas recomendadas por React Native
+    'plugin:prettier/recommended', // Integra Prettier con ESLint
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSameLine: true,
+        bracketSpacing: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'avoid',
+        endOfLine: 'auto', // Para manejar errores de 'CRLF' en Windows
+      },
+    ],
+  },
 };
